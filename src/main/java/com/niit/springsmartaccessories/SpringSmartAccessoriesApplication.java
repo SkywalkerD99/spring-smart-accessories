9f94e753d861dfbca1052dae699234d01c164e15
Pap. I. FY <=>
2021-10-15 10:36:01 +0000

    First Commit

---
+++ src/main/java/com/niit/springsmartaccessories/SpringSmartAccessoriesApplication.java
@@ -0,0 +1,13 @@
+package com.niit.springsmartaccessories;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSmartAccessoriesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSmartAccessoriesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/niit/springsmartaccessories/SpringSmartAccessoriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niit.springsmartaccessories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSmartAccessoriesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
