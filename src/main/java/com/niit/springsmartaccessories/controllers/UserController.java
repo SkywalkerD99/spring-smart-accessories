b64717237a8e651e5a144ddd5cb667372ed17736
Pap. I. FY <=>
2021-10-18 15:49:24 +0000

    Created UserDto and included into UserService. Created UserController

---
+++ src/main/java/com/niit/springsmartaccessories/controllers/UserController.java
@@ -0,0 +1,23 @@
+package com.niit.springsmartaccessories.controllers;
+
+import com.niit.springsmartaccessories.dto.UserDto;
+import com.niit.springsmartaccessories.services.UserService;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    public void createUser(@RequestBody UserDto userDto) {
+        userService.save(userDto);
+    }
+
+}
--- src/main/java/com/niit/springsmartaccessories/services/UserService.java
+++ src/main/java/com/niit/springsmartaccessories/services/UserService.java
@@ -1,5 +1,7 @@
 package com.niit.springsmartaccessories.services;
 
+import com.niit.springsmartaccessories.dto.UserDto;
+import com.niit.springsmartaccessories.models.User;
 import com.niit.springsmartaccessories.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,5 +13,13 @@ public class UserService {
 
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    public void save(UserDto userDto) {
+        for (User usr: userRepository.findAll())
+            if (userDto.getEmail().equals(usr.getEmail()))
+                throw new RuntimeException("User with email: " + usr.getEmail() + "already exists!");
+
+        userRepository.save(new User(userDto.getFirstname(), userDto.getLastname(), userDto.getEmail(), userDto.getAddress()));
     }
 }
